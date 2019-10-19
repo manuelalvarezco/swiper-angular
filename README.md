49f4a8409993e5c73c05e1256c5cecfefbf7b2c0
Manuel Alvarez <manuelalvarez.frontend@gmail.com>
2019-10-19 15:06:50 -0500

    Swiper funcionando

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Para instalar las dependencias se debe correr el comando`npm install`
 
 ## Code scaffolding
 Comando `ng serve` pacra correr el desarrollo en `http://localhost:4200/`. 
+
+## Procedimiento
+
+- Se instala la librería de Swiper `npm install swipe --save`
+- Se incluye la librería en los estilos del angular.json `./node_modules/swiper/css/swiper.css`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { ExampleService } from './services/example.service';
+
+import Swiper from "swiper";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'swiper-angular';
+export class AppComponent implements AfterViewInit {
+
+  mySwiper:Swiper;
+  
+  paises:any[] = [];
+
+  constructor(private _exampleService:ExampleService){
+
+    this._exampleService.getPaises()
+      .subscribe( (data:any)=>{
+        console.log(data);
+        this.paises = data;
+      })
+  }
+
+  ngAfterViewInit(){
+    this.mySwiper = new Swiper('.swiper-container')
+  }
+
 }
